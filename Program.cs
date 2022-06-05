f2f7fca57e49c4e540268136216672b1a270a8f1
Filip Madzia <filipmadzia33@gmail.com>
2022-06-05 17:51:42 +0200

    Pliki projektu

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sortowanie_bąbelkowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 4, 6};
+
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
